b2e47d859a60f89adf7e07e0e1435faaa3b39be0
AusMatthewYuen <matthewyuen@live.com>
2020-11-26 19:22:12 +1100

    Create server_turtle_miner.lua

---
+++ server_turtle_miner.lua
@@ -0,0 +1,29 @@
+slots = 16
+
+function string:split(delimiter)
+  local result = { }
+  local from  = 1
+  local delim_from, delim_to = string.find( self, delimiter, from  )
+  while delim_from do
+    table.insert( result, string.sub( self, from , delim_from-1 ) )
+    from  = delim_to + 1
+    delim_from, delim_to = string.find( self, delimiter, from  )
+  end
+  table.insert( result, string.sub( self, from  ) )
+  return result
+end
+
+turtle.suckDown(1)
+turtle.place()
+panda = turtle.inspect()
+print("panda")
+newTurtle = peripheral.wrap("front")
+newTurtle.turnOn()
+os.sleep(15)
+turtle.dig()
+turtle.turnLeft()
+
+for i in slots do 
+	turtle.drop(i)
+	
+turtle.turnRight()
