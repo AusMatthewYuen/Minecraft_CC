406945049f8a523345ed2b794aec3a188f640073
AusMatthewYuen <matthewyuen@live.com>
2020-11-28 16:44:17 +1100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from datetime import datetime
+from flask import stream_with_context
 app = Flask(__name__)
 
 import psycopg2
@@ -36,6 +37,9 @@ def gps_link():
 def mining_request():
     df_mining_result = pd.read_sql_query(con = engine, sql = """  select * from "Mining".mining_jobs order by time desc limit 1""")
     
+    if df_mining_result.empty == True:
+        return str("no jobs")
+    
     x = df_mining_result['x'][0]
     y = df_mining_result['y'][0]
     z = df_mining_result['z'][0]
@@ -52,10 +56,31 @@ def mining_request():
     and time = '{3}'
     """.format(x,y,z,time))
     
-    return str((x,y,z,x_quarry,y_quarry,z_quarry))
+    returned_string = str((x,y,z,x_quarry,y_quarry,z_quarry))
+    
+    returned_string = returned_string.replace('(' , "" )
+    returned_string = returned_string.replace(')' , "" )
+    returned_string = returned_string.replace("'" , "" )
+    returned_string = returned_string.replace(" " , "" )
+    
+    return returned_string
 
+@app.route('/mining_request')
+def mining_queue_add():
+    x = int(request.args.get('x')) #if key doesn't exist, returns None
+    y = int(request.args.get('y'))
+    z = int(request.args.get('z'))
+    xquarry = int(request.args.get('xquarry'))
+    yquarry = int(request.args.get('yquarry'))
+    zquarry = int(request.args.get('zquarry'))
+    numbots = int(request.args.get('numbots'))
+    
+    mining_job_splitter(x,y,z,xquarry,yquarry,zquarry,numbots)
+    
+    return ('job added to queue')
+        
 
-def mining_job_splitter(x, y ,z , xquarry = 10 ,yquarry = 5,zquarry = 5,numbots = 1):
+def mining_job_splitter(x, y ,z , xquarry = 50 ,yquarry = 1,zquarry = 1,numbots = 2):
     
     remainder = xquarry % numbots
     multiple = xquarry // numbots
@@ -64,16 +89,18 @@ def mining_job_splitter(x, y ,z , xquarry = 10 ,yquarry = 5,zquarry = 5,numbots 
     headers = ['job','x','y','z','x_quarry','y_quarry','z_quarry','time']
     
     mining_jobs = []
-    mining_range = range(numbots - 1)
+    mining_range = range(numbots)
     
     for i in mining_range:
         mining_jobs.append(('mining', x+(i * multiple), y, z,multiple  ,yquarry ,zquarry, timestamp))
         
-    if remainder != 0:
-        mining_jobs.append(('mining', x+(i * multiple), y, z,remainder  ,yquarry ,zquarry, timestamp))
+    if numbots != 1:
+        if remainder != 0:
+            mining_jobs.append(('mining', x+(i * multiple + 1 + multiple), y, z,remainder  ,yquarry ,zquarry, timestamp))
+        else:
+            mining_jobs.append(('mining', x+(i * multiple + 1 + multiple), y, z,multiple  ,yquarry ,zquarry, timestamp))
         
     df = pd.DataFrame(mining_jobs,columns = headers)
 
     df.to_sql( schema = 'Mining', name = 'mining_jobs',con = engine, if_exists = 'append')
 
-miner = mining_job_splitter(-100,57,23)
